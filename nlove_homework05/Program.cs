d4cae1d1f0bb75a45d4a3120c00381de9f4fd4e7
Natasha S Love <debora@inbox.ru>
2018-11-22 20:38:04 -0600

    Add project files.

---
+++ nlove_homework05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nlove_homework05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
